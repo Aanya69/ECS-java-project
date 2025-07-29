af1a6906a59c809d703e80e8266ac02736b22761
Aanya <aanya.raj@capgemini.com>
2025-07-29 23:38:06 +0530

    added java code

---
+++ ecsjavaapp/src/main/java/com/example1/ecsjavaapp/EcsjavaappApplication.java
@@ -0,0 +1,14 @@
+package com.example1.ecsjavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcsjavaappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcsjavaappApplication.class, args);
+	}
+
+}
+	
---
+++ ecsjavaapp/src/test/java/com/example1/ecsjavaapp/EcsjavaappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example1.ecsjavaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcsjavaappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
